lxqt/lxqt-qtplugin

commit d5f8558ee6add4c3db212957d1810a8afe3efbe8

LXQtPlatformTheme: Do not provide own palette
1. avoid leak by creating new object
2. we don't support overriding of colors -> default Qt palette is OK

src/lxqtplatformtheme.cpp
@@ -202,14 +202,6 @@ QPlatformDialogHelper *LXQtPlatformTheme::createPlatformDialogHelper(DialogType 
 }
 #endif
 
-const QPalette *LXQtPlatformTheme::palette(Palette type) const {
-
-    if (type == QPlatformTheme::SystemPalette)
-        // the default constructor uses the default palette
-        return new QPalette;
-    return QPlatformTheme::palette(type);
-}
-
 const QFont *LXQtPlatformTheme::font(Font type) const {
     if(type == SystemFont && !fontStr_.isEmpty()) {
         // NOTE: for some reason, this is not called by Qt when program startup.

src/lxqtplatformtheme.h
@@ -50,7 +50,7 @@ public:
     virtual bool usePlatformNativeDialog(DialogType type) const;
     // virtual QPlatformDialogHelper *createPlatformDialogHelper(DialogType type) const;
 
-    virtual const QPalette *palette(Palette type = SystemPalette) const;
+    // virtual const QPalette *palette(Palette type = SystemPalette) const;
 
     virtual const QFont *font(Font type = SystemFont) const;
 

commit 84954bdd7b899d29b30f45fab1c869d992d07279

LXQtPlatformTheme: Make Qt use default palette
Fixes forcing the Fusion palette even if other style engine used (as
the QPlatformTheme::palette() does force the fusion).

The mentioned regression was introduced by d5f8558ee6ad.

src/lxqtplatformtheme.cpp
@@ -207,6 +207,10 @@ QPlatformDialogHelper *LXQtPlatformTheme::createPlatformDialogHelper(DialogType 
 }
 #endif
 
+const QPalette *LXQtPlatformTheme::palette(Palette type) const {
+    return nullptr;
+}
+
 const QFont *LXQtPlatformTheme::font(Font type) const {
     if(type == SystemFont && !fontStr_.isEmpty()) {
         // NOTE: for some reason, this is not called by Qt when program startup.

src/lxqtplatformtheme.h
@@ -50,7 +50,7 @@ public:
     virtual bool usePlatformNativeDialog(DialogType type) const;
     // virtual QPlatformDialogHelper *createPlatformDialogHelper(DialogType type) const;
 
-    // virtual const QPalette *palette(Palette type = SystemPalette) const;
+    virtual const QPalette *palette(Palette type = SystemPalette) const;
 
     virtual const QFont *font(Font type = SystemFont) const;
 